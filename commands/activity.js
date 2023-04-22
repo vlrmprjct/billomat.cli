7211148826d3b99355fa2a9369694e5dd7a3a99f
Thomas M <thomas.meschke@billomat.com>
2023-04-22 21:24:28 +0200

    reorder activity table output, add timestamp

--- commands/activity.js
+++ commands/activity.js
@@ -1,38 +1,41 @@
-import { fetchAPI} from './../util/index.js';
+import { fetchAPI } from './../util/index.js';
 
-let prevId = '';
+let prevID = '';
 
 const activity = (args, id, token) => {
-
     fetchAPI(`activity-feed`, token, id, (data) => {
 
         args.forEach((arg) => {
             switch (arg) {
                 case '--watch':
-                    if (prevId !== data[0].id) {
-                        console.table({
-                            ID: data[0].id,
-                            Date: new Date(data[0].date).toLocaleString(),
-                            Type: data[0].resource,
-                            Title: data[0].title,
-                            Message: data[0].text,
-                            User: parseInt(data[0].user_id, 10),
-                        });
-                        prevId = data[0].id;
+                    if (prevID !== data[0].id) {
+                        console.table(
+                            [data[0]].map(({ id, date, resource, title, text, user_id }) => ({
+                                ID: id,
+                                Date: new Date(date).toLocaleString(),
+                                Timestamp: new Date(date).getTime() / 1000,
+                                Type: resource,
+                                Title: title,
+                                Message: text,
+                                User: parseInt(user_id, 10),
+                            }))
+                        );
+                        prevID = data[0].id;
                     }
                     break;
                 default:
                     if (args.length !== 1) return;
-                    data.map(({ id, date, resource, title, text, user_id }) => {
-                        console.table({
+                    console.table(
+                        data.map(({ id, date, resource, title, text, user_id }) => ({
                             ID: id,
                             Date: new Date(date).toLocaleString(),
+                            Timestamp: new Date(date).getTime() / 1000,
                             Type: resource,
                             Title: title,
                             Message: text,
                             User: parseInt(user_id, 10),
-                        });
-                    });
+                        }))
+                    );
                     break;
             }
         });
