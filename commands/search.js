297320192de045b0f5e7884ee49e580bbc9bce13
Thomas M <thomas.meschke@billomat.com>
2023-04-25 21:39:24 +0200

    [WIP] concepting search using tty-table

--- commands/search.js
+++ commands/search.js
@@ -3,38 +3,37 @@ import Table from 'tty-table';
 import { fetchAPI } from './../util/index.js';
 
 const options = {
-    borderStyle: "solid",
-    borderColor: "gray",
-    compact: false,
-    paddingBottom: 0,
-    headerAlign: "center",
-    headerColor: "green",
     align: "left",
+    borderColor: "gray",
+    borderStyle: "solid",
     color: "white",
-    width: "100%"
+    compact: false,
+    headerAlign: "center",
+    headerColor: "cyan",
+    truncate: ' ...',
+    width: "100%",
 }
 
 const search = (args, id, token) => {
 
-    const query = args[args.indexOf('--term') + 1];
-    const type = args[args.indexOf('--type') + 1];
-    const size = (args.indexOf('--size') < 0) ? 50 : args[args.indexOf('--size') + 1];
+    const {
+        query = args[args.indexOf('--term') + 1],
+        type = args[args.indexOf('--type') + 1],
+        size = (args.indexOf('--size') < 0) ? 50 : args[args.indexOf('--size') + 1],
+    } = args;
 
     const actions = {
         '--term': () => {
             fetchAPI(`search?query=${query}&resource=${type}&per_page=${size}`, token, id, (data) => {
-                console.table(
-                    data.map(({ id, resource, headline, subline }) => ({
-                        ID: id,
-                        Type: resource,
-                        Headline: headline,
-                        Subline: subline,
-                    }))
-                );
-
-                const t1 = Table(Object.keys(data[0]).map(key => ({ value: key })), data, options).render();
-                console.log(t1)
-
+                const header = [
+                    { value: 'id', alias: 'ID'},
+                    { value: 'resource', alias: 'Type'},
+                    { value: 'headline', alias: 'Number', width: 30, formatter: (value) => value.replace('[', '').split(']')[0] || ''},
+                    { value: 'headline', alias: 'Description', width: 30, formatter: (value) => value.replace('[', '').split(']')[1] || ''},
+                    { value: 'subline', alias: 'Excerpt' }
+                ];
+                const output = Table(header, data, options);
+                console.log(data.length > 0 ? output.render() + '\n' : '\n ❌ No results \n')
             });
         },
     };
