cfc9c900154b102b81dd35cf5a04442fca4e376f
Thomas Meschke <thomas.meschke@billomat.com>
2023-04-28 15:08:34 +0200

    [WIP] add columns

--- commands/resource.js
+++ commands/resource.js
@@ -1,43 +1,29 @@
 import { fetchAPI, table } from './../util/index.js';
 
+const columns = {
+    articles: [
+        { value: 'id', alias: 'ID', color: 'cyan' },
+        { value: 'article_number', alias: 'Number', color: 'cyan' },
+        { value: 'title', alias: 'Title', width: 30 },
+        { value: 'description', alias: 'Description', width: 50 },
+        { value: 'turnover', alias: 'Turnover', align: 'right', color: 'yellow', formatter: (value) => value.toFixed(2) },
+        { value: 'archived', alias: 'Archive', align: 'center', color: 'magenta', formatter: (value) => value ? '■' : '' },
+        { value: 'updated', alias: 'Changed', formatter: (value) => new Date(value).toLocaleString() },
+    ],
+    clients: [
+        { value: 'id', alias: 'ID', color: 'cyan' },
+    ]
+};
+
 const resource = (args, id, token) => {
 
     const {
-        query = args[args.indexOf('--term') + 1],
+        order = args[args.indexOf('--order') + 1],
         type = args[args.indexOf('--type') + 1],
         size = args[args.indexOf('--size') + 1],
     } = args;
 
-    console.log(args);
-
-    fetchAPI(type, token, id, (data) => {
-
-        const columns2 = [data[0]].map((x, i) => {
-            console.log(x)
-        })
-
-        columns2;
-
-        const columns = {
-            articles: [
-                { value: 'id', alias: 'ID', color: 'cyan' },
-                { value: 'date', alias: 'Date', formatter: (value) => new Date(value).toLocaleString() },
-                { value: 'date', alias: 'Timestamp', formatter: (value) => new Date(value).getTime() / 1000, width: 12 },
-                { value: 'user_id', alias: 'User', formatter: (value) => value || 'SYS', color: 'yellow' },
-                { value: 'resource', alias: 'Type' },
-                { value: 'title', alias: 'Title', width: 30 },
-                { value: 'text', alias: 'Message', width: 50 },
-            ],
-            clients: [
-                { value: 'id', alias: 'ID', color: 'red' },
-                { value: 'date', alias: 'Date', formatter: (value) => new Date(value).toLocaleString() },
-                { value: 'date', alias: 'Timestamp', formatter: (value) => new Date(value).getTime() / 1000, width: 12 },
-                { value: 'user_id', alias: 'User', formatter: (value) => value || 'SYS', color: 'yellow' },
-                { value: 'resource', alias: 'Type' },
-                { value: 'title', alias: 'Title', width: 30 },
-                { value: 'text', alias: 'Message', width: 50 },
-            ]
-        };
+    fetchAPI(`${type}?order_by=${order}&per_page=${size}`, token, id, (data) => {
 
         const output = table(data, columns[type]);
         console.log(data.length > 0 ? output + '\n' : '\n ❌ No results \n');
