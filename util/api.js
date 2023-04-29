6ac929bc02033a959a33d9b527b4934f4fa0e1f5
Thomas M <thomas.meschke@billomat.com>
2023-04-29 19:05:59 +0200

    cleanup api util

--- util/api.js
+++ util/api.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
-import { loaderStart, loaderStop } from './loader.js';
+import { colorize, loaderStart, loaderStop } from './index.js';
 
 const API_URL = process.env.NODE_ENV === 'development' ? 'billodev' : 'billomat';
 
-const fetchAPI = async (apiUrl, token, id, callback, options = {}) => {
+const fetchAPI = async (endpoint, token, id, callback, options = {}) => {
 
     const loaderInterval = loaderStart();
 
@@ -17,11 +17,11 @@ const fetchAPI = async (apiUrl, token, id, callback, options = {}) => {
     };
 
     try {
-        const response = await fetch(`https://${id}.${API_URL}.net/api/v2/${apiUrl}`, mapOptions);
+        const response = await fetch(`https://${id}.${API_URL}.net/api/v2/${endpoint}`, mapOptions);
 
         if (response.status !== 200) {
             loaderStop(loaderInterval);
-            console.log(response.statusText);
+            console.log(colorize(`\n ${response.status} : ${response.statusText} \n`, 'red'));
             return;
         }
 
