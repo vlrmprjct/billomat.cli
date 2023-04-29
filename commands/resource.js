06979c9c62bba7fb72b3c48d3e3f58eb984fd76c
Thomas M <thomas.meschke@billomat.com>
2023-04-29 19:23:54 +0200

    add resources

--- commands/resource.js
+++ commands/resource.js
@@ -1,4 +1,9 @@
-import { fetchAPI, table } from './../util/index.js';
+import {
+    argValue,
+    argRequired,
+    fetchAPI,
+    table,
+} from './../util/index.js';
 
 const columns = {
     articles: [
@@ -12,23 +17,37 @@ const columns = {
     ],
     clients: [
         { value: 'id', alias: 'ID', color: 'cyan' },
+        { value: 'client_number', alias: 'Number', color: 'cyan' },
+        { value: 'name', alias: 'Name', formatter: (value) => value.replace(/\\|\n|\r/g, ' ')},
+        { value: 'email', alias: 'Email' },
+        { value: 'turnover', alias: 'Turnover', align: 'right', color: 'yellow', formatter: (value) => value.toFixed(2) },
+        { value: 'archived', alias: 'Archive', align: 'center', color: 'magenta', formatter: (value) => value ? '■' : '' },
+        { value: 'updated', alias: 'Changed', formatter: (value) => new Date(value).toLocaleString() },
+    ],
+    suppliers: [
+        { value: 'id', alias: 'ID', color: 'cyan' },
+        { value: 'client_number', alias: 'Number', color: 'cyan' },
+        { value: 'name', alias: 'Name', formatter: (value) => value.replace(/\\|\n|\r/g, ' ')},
+        { value: 'email', alias: 'Email' },
+        { value: 'updated', alias: 'Changed', formatter: (value) => new Date(value).toLocaleString() },
     ]
 };
 
 const resource = (args, id, token) => {
 
     const {
-        order = args[args.indexOf('--order') + 1],
-        type = args[args.indexOf('--type') + 1],
-        size = args[args.indexOf('--size') + 1],
+        order = argValue(args, '--sort', 'id'),
+        page = argValue(args, '--page', '1'),
+        type = argValue(args, '--type'),
+        size = argValue(args, '--size', '100'),
     } = args;
 
-    fetchAPI(`${type}?order_by=${order}&per_page=${size}`, token, id, (data) => {
+    if (!argRequired(args, ['--type'], { '--type': ['articles', 'clients', 'suppliers'] })) return;
 
-        const output = table(data, columns[type]);
-        console.log(data.length > 0 ? output + '\n' : '\n ❌ No results \n');
+    fetchAPI(`${type}?order_by=${order}&per_page=${size}&page=${page}`, token, id, (data) => {
 
-        console.log('Count: ', data.length + '\n');
+        const result = data.length > 0 ? table(data, columns[type]) + '\n' : '\n ❌ No results \n';
+        console.log(result);
 
     });
 };
